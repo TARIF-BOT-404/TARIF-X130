360fd95e3932cf925c74c6ef66c665d58f73bba9
MR-TARIF-YT130 <mrtarifyt.x130@gmail.com>
2024-11-21 10:33:47 +0600

    Update wallpaper.js

--- scripts/cmds/wallpaper.js
+++ scripts/cmds/wallpaper.js
@@ -4,7 +4,7 @@ const path = require("path");
 module.exports = {
   config: {
     name: "wallpaper",
-    aliases: ["wl"],
+    aliases: ["wll"],
     version: "1.1",
     author: "Samir Œ",
     countDown: 5,
